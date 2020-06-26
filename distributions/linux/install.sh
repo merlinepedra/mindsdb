3fa551c5fadead66be46429754522a0e16b509d3
Борис Цейтлин <euphetar@gmail.com>
2020-06-26 12:09:08 +0300

    Remove configuration setting, remove user input

--- distributions/linux/install.sh
+++ distributions/linux/install.sh
@@ -33,60 +33,28 @@ $cmdcol
 
 """
 
+printf "Installing MindsDb requires Python 3.6+ and pip. We will now attempt to detect their paths."
 # Attempt to detect python
 python_path="$(which python3)"
 pip_path="$(which pip3)"
-printf "Detected python3: $python_path\ndetected pip3: $pip_path\nDo you want to use these [Y] or manually provide paths [N]? [Y/N]"
-read use_detected_python
-if [ "$use_detected_python" = "N" ] || [ "$use_detected_python" = "n" ]; then
-    echo "Please enter the path to your python (3.6+) interpreter:"
-    read python_path
-
-    echo "Please enter the path to your associate pip installation:"
-    read pip_path
+if [ -z "${python_path}" ]
+then
+    python_path="$(which python)"
 fi
-export MDB_INSTALL_PYTHONPATH="$python_path"
-export MDB_INSTALL_PIPPATH="$pip_path"
+
+if [ -z "${pip_path}" ]
+then
+    pip_path="$(which pip)"
+fi
+
+printf "Detected python: $python_path\ndetected pip: $pip_path"
 
 # Check that it's indeed python 3.6 and that pip works
 ${python_path} -c "import sys; print('Sorry, MindsDB requires Python 3.6+') and exit(1) if sys.version_info < (3,6) else exit(0)"
 ${pip_path} --version > /dev/null 2>&1
 
-echo "Do you want us to install using the default parameters [Y] or should we go ahead and give you control to tweak stuff during installation ? [Y/N]"
-read default_install
-export MDB_DEFAULT_INSTALL="$default_install"
-
-export MDB_MAKE_EXEC="Y"
-if [ "$EUID" -ne 0 ]; then
-  install_as="user"
-else
-  install_as="global"
-fi
-
-if [ "$default_install" = "N" ] || [ "$default_install" = "n" ]; then
-  if [ "$EUID" -ne 0 ]; then
-      install_as="user"
-      echo "You are currently installing Mindsdb for your user only, rather than globally. Is this intended ? [Y/N]"
-      read approve
-      if [ "$approve" = "N" ] || [ "$approve" = "n" ]; then
-          echo "Please run the installer using sudo in front of the command"
-          exit
-      fi
-    else
-      install_as="global"
-      echo "You are currently installing Mindsdb globally (as root), is this intended ? [Y/N]"
-      read approve
-      if [ "$approve" = "N" ] || [ "$approve" = "n" ]; then
-          echo "Please run the installer as your desired user instead (without using sudo in front of it)"
-          exit
-      fi
-  fi
-
-  echo "Should we make an executable for mindsdb (in /usr/bin/ if installing as root or in your home directory if install as user)? [Y/N]"
-  read make_exec
-  export MDB_MAKE_EXEC="$make_exec"
-fi
-
+export MDB_INSTALL_PYTHONPATH="$python_path"
+export MDB_INSTALL_PIPPATH="$pip_path"
 
 cmdcol="$(tput sgr0)$(tput bold)"
 normalcol="$(tput sgr0)"
@@ -122,10 +90,14 @@ $cmdcol
 
 """
 
+temp_file=$(mktemp)
+trap "rm -f $temp_file" 0 2 3 15 #Making sure the file is deleted after script finishes
+
 # Python code below
-cat << EOF > _install.py
+cat << EOF > $temp_file
 #!$python_path
 
+import traceback
 import sys
 import os
 from pathlib import Path
@@ -133,46 +105,18 @@ import time
 from os.path import expanduser
 
 
-install_as  = sys.argv[1]
-python_path = sys.argv[2]
-pip_path    = sys.argv[3]
-default_install = sys.argv[4]
-make_exec = sys.argv[5]
+python_path = sys.argv[1]
+pip_path    = sys.argv[2]
 home = expanduser("~")
 mdb_home = os.path.join(home, 'mindsdb')
 
-default_install = False if default_install.lower() == 'n' else True
-make_exec = False if make_exec.lower() == 'n' else True
-
-
-if install_as == 'user':
-    config_dir = os.path.join(mdb_home,'data', 'config')
-    storage_dir = os.path.join(mdb_home,'data', 'storage')
-else:
-    config_dir = os.path.join('/etc/mindsdb/')
-    storage_dir = os.path.join('/var/lib/mindsdb/')
-
-os.makedirs(config_dir,exist_ok=True)
-os.makedirs(storage_dir,exist_ok=True)
-
-print(f'Configuration files will be stored in {config_dir}')
-print(f'Datasources and predictors will be stored in {storage_dir}')
-
-datasource_dir = os.path.join(storage_dir,'datasources')
-predictor_dir = os.path.join(storage_dir,'predictors')
-
-os.makedirs(datasource_dir,exist_ok=True)
-os.makedirs(predictor_dir,exist_ok=True)
 
 print(f'\nInstalling some large dependencies via pip ({pip_path}), this might take a while\n')
-time.sleep(3)
+time.sleep(1)
 
-# Consider adding:  --force-reinstall
-# How it installs itself (maybe instead of github just use local download if user has cloned everything ?)
-if install_as == 'user':
-    os.system(f'{pip_path} install  git+https://github.com/mindsdb/mindsdb_server.git@split --upgrade')
-else:
-    os.system(f'sudo {pip_path} install git+https://github.com/mindsdb/mindsdb_server.git@split --upgrade')
+retcode = os.system(f'{pip_path} install  git+https://github.com/mindsdb/mindsdb_server.git@split --upgrade')
+if retcode != 0:
+    raise(Exception("Command exited with error"))
 
 dataskillet_source = None
 lightwood_source = f'git+https://github.com/mindsdb/lightwood.git@stable'
@@ -180,45 +124,32 @@ mindsdb_native_source = f'git+https://github.com/mindsdb/mindsdb_native.git@stab
 
 for source in [dataskillet_source,lightwood_source,mindsdb_native_source]:
     if isinstance(source,str):
-        if install_as == 'user':
-            os.system(f'{pip_path} install {source} --upgrade')
-        else:
-            os.system(f'sudo {pip_path} install {source} --upgrade')
+        retcode = os.system(f'{pip_path} install {source} --upgrade')
+        if retcode != 0:
+            raise(Exception("Command exited with error"))
 time.sleep(1)
 print('Done installing dependencies')
 print('\nLast step: Configure Mindsdb\n')
 
-from mindsdb_server.utilities.wizards import cli_config,daemon_creator,make_executable
-config_path = cli_config(python_path,pip_path,predictor_dir,datasource_dir,config_dir,use_default=default_install)
+from mindsdb_server.utilities.wizards import daemon_creator, make_executable
 
-if install_as == 'user':
-    pass
-else:
-    daemon_creator(python_path,config_path)
+daemon_path = daemon_creator(python_path)
+print(f"Created daemon service config {daemon_path}")
 
-if make_exec:
-    if install_as == 'user':
-        path = str(os.path.join(mdb_home,'run'))
-    else:
-        path = '/usr/bin/mindsdb'
+exec_path = str(os.path.join(mdb_home,'run'))
+make_executable(python_path, exec_path)
+print(f"Created executable at {exec_path}")
 
-    make_executable(python_path,config_path,path)
+print('Installation complete!')
 
-print('Installation complete !')
-
-if make_exec:
-    print(f'You can use Mindsdb by running {path}. Or by importing it as the mindsdb library from within python.')
-else:
-    print(f'You can use Mindsdb by running {python_path} -m mindsdb_server --config={config_path}. Or by importing it as the mindsdb library from within python.')
+print(f'You can use Mindsdb by running {exec_path}. Or by importing it as the mindsdb_native library from within python.')
 
 
 EOF
 #/Python code
 
-chmod 755 _install.py
+chmod 755 $temp_file
 
 INSTALLER_SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-"${MDB_INSTALL_PYTHONPATH}" "$INSTALLER_SCRIPT_DIR"/_install.py "$install_as" "$MDB_INSTALL_PYTHONPATH" "$MDB_INSTALL_PIPPATH" "$MDB_DEFAULT_INSTALL" "$MDB_MAKE_EXEC"
-
-rm _install.py
+"${MDB_INSTALL_PYTHONPATH}" "$temp_file" "$MDB_INSTALL_PYTHONPATH" "$MDB_INSTALL_PIPPATH"
