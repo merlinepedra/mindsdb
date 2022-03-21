8eaa7a8e10c87a5a2402b696aecb97d63dede5c9
Patricio Cerda Mardini <pcerdam@live.com>
2022-03-21 17:13:59 +0100

    add handler tagging

--- mindsdb/integrations/mlflow/mlflow/mlflow_integration.py
+++ mindsdb/integrations/mlflow/mlflow/mlflow_integration.py
@@ -72,7 +72,7 @@ class MLflowIntegration(PredictiveHandler):
         cur = self.internal_registry.cursor()
         if ('models',) not in list(cur.execute("SELECT name FROM sqlite_master WHERE type='table';")):
             cur.execute("""create table models (model_name text, format text, target text, url text)""")  # TODO: dtype_dict?
-        self.internal_registry.commit()
+            self.internal_registry.commit()
 
     def check_status(self):
         """ Checks that the connection is, as expected, an MlflowClient instance. """  # noqa
@@ -132,7 +132,6 @@ class MLflowIntegration(PredictiveHandler):
         # anything is stored, just providing the context to company/users
             # e.g. lightwood: save/load models, store metadata about models, all that is delegated to mdb.
 
-        # todo
         statement = self.parser(query_str, dialect=self.dialect)  # one of mindsdb_sql:mindsdb dialect types
 
         if type(statement) == CreatePredictor:
@@ -161,9 +160,8 @@ class MLflowIntegration(PredictiveHandler):
 
         elif type(statement) == DropPredictor:
             predictor_name = statement.name.parts[-1]
-            session.datahub['mindsdb'].delete_predictor(predictor_name)
             cur = self.internal_registry.cursor()
-            cur.execute("""DELETE FROM models WHERE model_name='{predictor_name}'""")
+            cur.execute(f"""DELETE FROM models WHERE model_name='{predictor_name}'""")
             self.internal_registry.commit()
 
         else:
@@ -178,7 +176,7 @@ class MLflowIntegration(PredictiveHandler):
         """  # noqa
         _, _, target, model_url = self._get_model(stmt)
 
-        if target not in [str(t) for t in parsed.targets]:
+        if target not in [str(t) for t in stmt.targets]:
             raise Exception("Predictor will not be called, target column is not specified.")
 
         df = pd.DataFrame.from_dict({stmt.where.args[0].parts[0]: [stmt.where.args[1].value]})
@@ -193,22 +191,49 @@ class MLflowIntegration(PredictiveHandler):
         Within this method one should specify specialized logic particular to how the framework would pre-process
         data coming from a datasource integration, before actually passing the selected dataset to the model.
         """  # noqa
-        data_handler_table = stmt.from_table.left.parts[-1]  # todo should be ".".join(parsed.from_table.left.parts) once data handler supports more than one table
-        data_handler_cols = list(set([t.parts[-1] for t in stmt.targets]))
 
-        # integration should handle this depending on type of query... e.g. if it is TS, then we have to fetch correct groups first and limit later
-        limit = stmt.limit.value
+        # tag data and predictive handlers
+        if len(stmt.from_table.left.parts) == 1:
+            model_clause = 'left'
+            data_clause = 'right'
+        else:
+            model_clause = 'right'
+            data_clause = 'left'
+        model_alias = str(getattr(stmt.from_table, model_clause).alias)
+
+        # get model input
+        data_handler_table = getattr(stmt.from_table, data_clause).parts[-1]  # todo should be ".".join(...) once data handler supports more than one table
+        data_handler_cols = list(set([t.parts[-1] for t in stmt.targets]))
 
         data_query = f"""SELECT {','.join(data_handler_cols)} FROM {data_handler_table}"""
         if stmt.where:
             data_query += f" WHERE {str(stmt.where)}"
         if stmt.limit:
-            data_query += f" LIMIT {limit}"
+            # integration should handle this depending on type of query... e.g. if it is TS, then we have to fetch correct groups first and limit later
+            data_query += f" LIMIT {stmt.limit.value}"
 
         model_input = pd.DataFrame.from_records(data_handler.run_native_query(data_query))
-        _, _, _, model_url = self._get_model(stmt)
 
-        return self._call_model(model_input, model_url)
+        # rename columns
+        aliased_columns = list(model_input.columns)
+        for col in stmt.targets:
+            if str(col.parts[0]) != model_alias and col.alias is not None:
+                # assumes mdb_sql will alert if there are two columns with the same alias
+                aliased_columns[aliased_columns.index(col.parts[-1])] = str(col.alias)
+        model_input.columns = aliased_columns
+
+        # get model output
+        _, _, _, model_url = self._get_model(stmt)
+        predictions = self._call_model(model_input, model_url)
+
+        # rename columns
+        aliased_columns = list(predictions.columns)
+        for col in stmt.targets:
+            if col.parts[0] == model_alias and col.alias is not None:
+                aliased_columns[aliased_columns.index('prediction')] = str(col.alias)
+        predictions.columns = aliased_columns
+
+        return predictions
 
     def _get_model(self, stmt):
         if type(stmt.from_table) == Join:
@@ -243,39 +268,40 @@ class MLflowIntegration(PredictiveHandler):
 if __name__ == '__main__':
     # TODO: turn this into tests
 
-    registered_model_name = 'nlp_kaggle3'  # already saved to mlflow local instance
-    cls = MLflowIntegration('test_mlflow')
-    print(cls.connect(
-        mlflow_url='http://127.0.0.1:5001',  # for this test, serve at 5001 and served model at 5000
-        model_registry_path='sqlite:////Users/Pato/Work/MindsDB/temp/experiments/BYOM/mlflow.db'))
-    try:
-        cls.run_native_query(f"DROP PREDICTOR {registered_model_name}")
-    except:
-        pass
-    query = f"CREATE PREDICTOR {registered_model_name} PREDICT target USING url.predict='http://localhost:5000/invocations', format='mlflow', dtype_dict={{'text': 'rich_text', 'target': 'binary'}}"
-    cls.run_native_query(query)
-    print(cls.get_tables())
-    print(cls.describe_table(f'{registered_model_name}'))
+    if True:
+        registered_model_name = 'nlp_kaggle3'  # already saved to mlflow local instance
+        cls = MLflowIntegration('test_mlflow')
+        print(cls.connect(
+            mlflow_url='http://127.0.0.1:5001',  # for this test, serve at 5001 and served model at 5000
+            model_registry_path='sqlite:////Users/Pato/Work/MindsDB/temp/experiments/BYOM/mlflow.db'))
+        try:
+            cls.run_native_query(f"DROP PREDICTOR {registered_model_name}")
+        except:
+            pass
+        query = f"CREATE PREDICTOR {registered_model_name} PREDICT target USING url.predict='http://localhost:5000/invocations', format='mlflow', dtype_dict={{'text': 'rich_text', 'target': 'binary'}}"
+        cls.run_native_query(query)
+        print(cls.get_tables())
+        print(cls.describe_table(f'{registered_model_name}'))
 
-    # Tests with MySQL handler: JOIN
-    from mindsdb.integrations.mysql_handler.mysql_handler.mysql_handler import MySQLHandler  # expose through parent init
-    kwargs = {
-        "host": "localhost",
-        "port": "3306",
-        "user": "root",
-        "password": "root",
-        "database": "test",
-        "ssl": False
-    }
-    sql_handler_name = 'test_handler'
-    data_table_name = 'train_escaped_csv' # 'tweet_sentiment_train'
-    handler = MySQLHandler(sql_handler_name, **kwargs)
-    assert handler.check_status()
+        # Tests with MySQL handler: JOIN
+        from mindsdb.integrations.mysql_handler.mysql_handler.mysql_handler import MySQLHandler  # expose through parent init
+        kwargs = {
+            "host": "localhost",
+            "port": "3306",
+            "user": "root",
+            "password": "root",
+            "database": "test",
+            "ssl": False
+        }
+        sql_handler_name = 'test_handler'
+        data_table_name = 'train_escaped_csv' # 'tweet_sentiment_train'
+        handler = MySQLHandler(sql_handler_name, **kwargs)
+        assert handler.check_status()
 
-    query = f"SELECT target from {registered_model_name} WHERE text='This is nice.'"
-    parsed = cls.parser(query, dialect=cls.dialect)
-    predicted = cls.select_query(parsed)
+        query = f"SELECT target from {registered_model_name} WHERE text='This is nice.'"
+        parsed = cls.parser(query, dialect=cls.dialect)
+        predicted = cls.select_query(parsed)
 
-    query = f"SELECT ta.target as predicted, tb.target as real, tb.text from {sql_handler_name}.{data_table_name} AS ta JOIN {registered_model_name} AS tb LIMIT 10"
-    parsed = cls.parser(query, dialect=cls.dialect)
-    predicted = cls.join(parsed, handler)
+        query = f"SELECT tb.target as predicted, ta.target as real, tb.text from {sql_handler_name}.{data_table_name} AS ta JOIN {registered_model_name} AS tb LIMIT 10"
+        parsed = cls.parser(query, dialect=cls.dialect)
+        predicted = cls.join(parsed, handler)
