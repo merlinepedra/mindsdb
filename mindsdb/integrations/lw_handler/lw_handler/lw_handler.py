06d16174a5b2bc0ac4e4bc7b2e0411de6b4d674c
Patricio Cerda Mardini <pcerdam@live.com>
2022-03-29 18:29:47 +0200

    feat: main lightwood handler behaviors functional

--- mindsdb/integrations/lw_handler/lw_handler/lw_handler.py
+++ mindsdb/integrations/lw_handler/lw_handler/lw_handler.py
@@ -1,36 +1,22 @@
 import os
+import gc
 import dill
-import requests
-import torch.multiprocessing as mp
+import pandas as pd
+from typing import Dict, List, Optional
 
-from ast import literal_eval
-from typing import Dict, List, Union, Optional
-from datetime import datetime
-
-from lightwood.api.high_level import json_ai_from_problem, predictor_from_code, code_from_json_ai, ProblemDefinition
+from lightwood.api.high_level import json_ai_from_problem, predictor_from_code, code_from_json_ai, ProblemDefinition, _module_from_code
 
 from mindsdb.integrations.libs.base_handler import BaseHandler, PredictiveHandler
 from mindsdb.integrations.libs.storage_handler import SqliteStorageHandler
 from mindsdb.integrations.mysql_handler.mysql_handler.mysql_handler import MySQLHandler
 from mindsdb.utilities.config import Config
-from mindsdb import __version__ as mindsdb_version
-from mindsdb.utilities.functions import mark_process
-import mindsdb.interfaces.storage.db as db
-from mindsdb.interfaces.model.model_controller import ModelController
 from mindsdb_sql import parse_sql
 from mindsdb_sql.parser.ast import Join
 from mindsdb_sql.parser.dialects.mindsdb import (
-    CreateDatasource,
-    RetrainPredictor,
+    # RetrainPredictor,  # todo
     CreatePredictor,
-    DropDatasource,
-    DropPredictor,
-    CreateView
+    DropPredictor
 )
-
-import mlflow
-from mlflow.tracking import MlflowClient
-import pandas as pd
 
 
 class LightwoodHandler(PredictiveHandler):
@@ -72,9 +58,7 @@ class LightwoodHandler(PredictiveHandler):
         if table_name not in self.get_tables():
             print("Table not found.")
             return {}
-
-        model = self.storage.get('models')[model_name]
-        return model['jsonai']
+        return self.storage.get('models')[model_name]['jsonai']
 
     def run_native_query(self, query_str: str) -> Optional[object]:
         statement = self.parser(query_str, dialect=self.dialect)
@@ -82,11 +66,10 @@ class LightwoodHandler(PredictiveHandler):
         if type(statement) == CreatePredictor:
             model_name = statement.name.parts[-1]
 
-            # check that it exists within mlflow and is not already registered
             if model_name in self.get_tables():
-                print("Error: this model is already registered!")
+                print("Error: this model already exists!")
             else:
-                target = statement.targets[0].parts[-1]  # TODO: multiple target support?
+                target = statement.targets[0].parts[-1]
                 params = {
                     'target': target,
                 }
@@ -95,10 +78,11 @@ class LightwoodHandler(PredictiveHandler):
 
                 # get training data from other integration
                 # todo: MDB needs to expose all available handlers through some sort of global state
+                # todo: change data gathering logic if task is TS, use self._data_gather or similar
                 handler = MDB_CURRENT_HANDLERS[str(statement.integration_name)]
                 handler_query = self.parser(statement.query_str, dialect=self.handler_dialect)
                 records = handler.select_query(targets=handler_query.targets, from_stmt=handler_query.from_table, where_stmt=handler_query.where)
-                df = pd.DataFrame.from_records(records)[:10]
+                df = pd.DataFrame.from_records(records)[:10]  # todo remove forced cap
                 jsonai = json_ai_from_problem(df, ProblemDefinition.from_dict(params))
 
                 # todo inject with all params in using clause here
@@ -125,19 +109,22 @@ class LightwoodHandler(PredictiveHandler):
             raise Exception(f"Query type {type(statement)} not supported")
 
     def select_query(self, stmt) -> pd.DataFrame:
-        _, _, target, model_url = self._get_model(stmt)
-
-        if target not in [str(t) for t in stmt.targets]:
-            raise Exception("Predictor will not be called, target column is not specified.")
+        # todo: expand to arbitrary amount of values
+        model = self._get_model(stmt)
 
         df = pd.DataFrame.from_dict({stmt.where.args[0].parts[0]: [stmt.where.args[1].value]})
-        return self._call_model(df, model_url)
+        return self._call_predictor(df, model)
+
+    def _data_gather(self):
+        # todo: this would be the pipeline for specialized tasks
+        # e.g. check whether it's a TS task, then we gather data with the relevant query plan
+        # e.g. if it's an image task, we go and fetch all images
+        pass
 
     def join(self, stmt, data_handler: BaseHandler, into: Optional[str] = None) -> pd.DataFrame:
         """
         Batch prediction using the output of a query passed to a data handler as input for the model.
         """  # noqa
-
         # tag data and predictive handlers
         if len(stmt.from_table.left.parts) == 1:
             model_clause = 'left'
@@ -147,16 +134,14 @@ class LightwoodHandler(PredictiveHandler):
             data_clause = 'left'
         model_alias = str(getattr(stmt.from_table, model_clause).alias)
 
-        # get model input
-        data_handler_table = getattr(stmt.from_table, data_clause).parts[
-            -1]  # todo should be ".".join(...) if data handlers support more than one table
+        data_handler_table = getattr(stmt.from_table, data_clause).parts[-1]  # todo should be ".".join(...) if data handlers support more than one table
         data_handler_cols = list(set([t.parts[-1] for t in stmt.targets]))
 
         data_query = f"""SELECT {','.join(data_handler_cols)} FROM {data_handler_table}"""
         if stmt.where:
             data_query += f" WHERE {str(stmt.where)}"
         if stmt.limit:
-            # todo integration should handle this depending on type of query... e.g. if it is TS, then we have to fetch correct groups first and limit later
+            # todo integration should handle this depending on type of query... e.g. if it is TS, then we have to fetch correct groups first and limit later, use self._data_gather or similar
             data_query += f" LIMIT {stmt.limit.value}"
 
         parsed_query = self.parser(data_query, dialect=self.dialect)
@@ -176,8 +161,8 @@ class LightwoodHandler(PredictiveHandler):
         model_input.columns = aliased_columns
 
         # get model output
-        _, _, _, model_url = self._get_model(stmt)
-        predictions = self._call_model(model_input, model_url)
+        model = self._get_model(stmt)
+        predictions = self._call_predictor(model_input, model)
 
         # rename columns
         aliased_columns = list(predictions.columns)
@@ -200,33 +185,45 @@ class LightwoodHandler(PredictiveHandler):
         else:
             model_name = stmt.from_table.parts[-1]
 
-        mlflow_models = [model.name for model in self.connection.list_registered_models()]
         if not model_name in self.get_tables():
             raise Exception("Error, not found. Please create this predictor first.")
-        elif not model_name in mlflow_models:
-            raise Exception(
-                "Cannot connect with the model, it might not served. Please serve it with MLflow and try again.")
 
-        model = self.connection.get_registered_model(model_name)
-        model_info = self.storage.get('models')[model_name]
-        return model_name, model, model_info['target'], model_info['url']
+        predictor_dict = self._get_model_info(model_name)
+        predictor = self._load_predictor(predictor_dict, model_name)
+        return predictor
 
-    def _call_model(self, df, model_url):
-        resp = requests.post(model_url,
-                             data=df.to_json(orient='records'),
-                             headers={'content-type': 'application/json; format=pandas-records'})
-        answer: List[object] = resp.json()
+    def _get_model_info(self, model_name):
+        """ Returns a dictionary with three keys: 'jsonai', 'predictor' (serialized), and 'code'. """  # noqa
+        return self.storage.get('models')[model_name]
 
-        predictions = pd.DataFrame({'prediction': answer})
-        out = df.join(predictions)
-        return out
+    def _load_predictor(self, predictor_dict, name):
+        # todo update lightwood method (predictor_from_state) so that it can take a memory representation of the predictor object OR a file path, and call that instead
+        try:
+            module_name = None
+            return dill.loads(predictor_dict['predictor'])
+        except Exception as e:
+            module_name = str(e).lstrip("No module named '").split("'")[0]
+
+            try:
+                del sys.modules[module_name]
+            except Exception:
+                pass
+
+            gc.collect()
+            _module_from_code(predictor_dict['code'], module_name)
+            return dill.loads(predictor_dict['predictor'])
+
+    def _call_predictor(self, df, predictor):
+        predictions = predictor.predict(df)
+        return df.join(predictions)
 
 
 if __name__ == '__main__':
     # TODO: turn this into tests
 
+    data_handler_name = 'mysql_handler'
     MDB_CURRENT_HANDLERS = {
-        'mysql_handler': MySQLHandler('test_handler', **{
+        data_handler_name: MySQLHandler('test_handler', **{
             "host": "localhost",
             "port": "3306",
             "user": "root",
@@ -234,8 +231,9 @@ if __name__ == '__main__':
             "database": "test",
             "ssl": False
         })
-    }
-    print(MDB_CURRENT_HANDLERS['mysql_handler'].check_status())
+    }  # todo: handler CRUD should be done at mindsdb top-level
+    data_handler = MDB_CURRENT_HANDLERS[data_handler_name]
+    print(data_handler.check_status())
 
     cls = LightwoodHandler('LWtest')
     config = Config()
@@ -249,45 +247,36 @@ if __name__ == '__main__':
         pass
 
     print(cls.get_tables())
+
+    data_table_name = 'home_rentals_subset'
     model_name = 'lw_test_predictor'
+    target = 'rental_price'
     if model_name not in cls.get_tables():
-        query = f"CREATE PREDICTOR {model_name} FROM mysql_handler (SELECT * FROM test.home_rentals_subset ) PREDICT rental_price"
+        query = f"CREATE PREDICTOR {model_name} FROM {data_handler_name} (SELECT * FROM test.{data_table_name}) PREDICT {target}"
         cls.run_native_query(query)
 
     print(cls.describe_table(f'{model_name}'))
-    #
-    # # Tests with MySQL handler: JOIN
-    # from mindsdb.integrations.mysql_handler.mysql_handler.mysql_handler import \
-    #     MySQLHandler  # expose through parent init
-    #
-    # kwargs = {
-    #     "host": "localhost",
-    #     "port": "3306",
-    #     "user": "root",
-    #     "password": "root",
-    #     "database": "test",
-    #     "ssl": False
-    # }
-    # sql_handler_name = 'test_handler'
-    # data_table_name = 'train_escaped_csv'  # 'tweet_sentiment_train'
-    # handler = MySQLHandler(sql_handler_name, **kwargs)
-    # assert handler.check_status()
-    #
-    # query = f"SELECT target from {registered_model_name} WHERE text='This is nice.'"
-    # parsed = cls.parser(query, dialect=cls.dialect)
-    # predicted = cls.select_query(parsed)
-    #
-    # into_table = 'test_join_into_mlflow'
-    # query = f"SELECT tb.target as predicted, ta.target as real, tb.text from {sql_handler_name}.{data_table_name} AS ta JOIN {registered_model_name} AS tb LIMIT 10"
-    # parsed = cls.parser(query, dialect=cls.dialect)
-    # predicted = cls.join(parsed, handler, into=into_table)
-    #
-    # # checks whether `into` kwarg does insert into the table or not
-    # q = f"SELECT * FROM {into_table}"
-    # qp = cls.parser(q, dialect='mysql')
-    # assert len(handler.select_query(qp.targets, qp.from_table, qp.where)) > 0
-    #
-    # try:
-    #     handler.run_native_query(f"DROP TABLE test.{into_table}")
-    # except:
-    #     pass
+
+    query = f"SELECT target from {model_name} WHERE sqft=100"
+    parsed = cls.parser(query, dialect=cls.dialect)
+    predicted = cls.select_query(parsed)
+
+    into_table = 'test_join_into_lw'
+    query = f"SELECT tb.{target} as predicted, ta.{target} as truth, ta.sqft from {data_handler_name}.{data_table_name} AS ta JOIN {model_name} AS tb LIMIT 10"
+    parsed = cls.parser(query, dialect=cls.dialect)
+    predicted = cls.join(parsed, data_handler, into=into_table)
+
+    # checks whether `into` kwarg does insert into the table or not
+    q = f"SELECT * FROM {into_table}"
+    qp = cls.parser(q, dialect='mysql')
+    assert len(data_handler.select_query(qp.targets, qp.from_table, qp.where)) > 0
+
+    try:
+        data_handler.run_native_query(f"DROP TABLE test.{into_table}")
+    except:
+        pass
+
+    try:
+        cls.run_native_query(f"DROP PREDICTOR {model_name}")
+    except:
+        pass
